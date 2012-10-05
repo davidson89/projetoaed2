4cbb48483ea4486fe441854661fc26816240c35e
davidson.or@gmail.com <davidson.or@gmail.com@f537ab09-7209-2eb9-f7ed-ef966541a065>
2012-10-05 01:01:31 +0000

    Melhorando comentarios na classe main.cpp

--- trunk/projectHash/Les.cpp
+++ trunk/projectHash/Les.cpp
@@ -1,5 +1,9 @@
 #include "Les.h"
 
+/**
+ * Esta classe será ultilizada para compor a estrutura da HashTable, montando uma estrutura de lista ligada simples.
+ * @param valor string que sera armazenada na estrutura.
+**/
 Les::Les(string valor)
 {
     this->valor = valor;
